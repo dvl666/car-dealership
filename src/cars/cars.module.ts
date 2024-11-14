d3263df4b21df2af051568d7baced1bf4f47298d
dvl666 <kamagaspar@gmail.com>
2024-11-14 15:32:10 -0300

    add service and comments

--- src/cars/cars.module.ts
+++ src/cars/cars.module.ts
@@ -2,6 +2,10 @@ import { Module } from '@nestjs/common';
 import { CarsController } from './cars.controller';
 import { CarsService } from './cars.service';
 
+/**
+ * Todos los servicios son providers, no todos los providers son servicios
+ */
+
 @Module({
   controllers: [CarsController],
   providers: [CarsService]
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
-export class CarsService {}
+export class CarsService {
+
+    private cars = [
+        {
+            id: 1,
+            brand: 'Toyota',
+            model: 'Corolla',
+            year: 2019
+        },
+        {
+            id: 2,
+            brand: 'Toyota',
+            model: 'Yaris',
+            year: 2019
+        },
+        {
+            id: 3,
+            brand: 'Toyota',
+            model: 'Hilux',
+            year: 2019
+        },
+        {
+            id: 4,
+            brand: 'Toyota',
+            model: 'Rav4',
+            year: 2019
+        }
+    ];
+
+    findAll() {
+        return this.cars;
+    }
+    
+    getCarById(carId: number): {id, brand, model, year} {
+        const car = this.cars.find(car => car.id === carId);
+        /**
+         * Es una buena practica manejar las excepciones en el servicio
+         */
+        if (!car) throw new NotFoundException(`Car with id '${carId}' not found`);
+        return car
+    }
+
+}
