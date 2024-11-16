165fbd01d1481b944a2c56a4ca4111fc2066bd21
dvl666 <kamagaspar@gmail.com>
2024-11-16 03:21:50 -0300

    update aoo module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CarsModule } from './cars/cars.module';
 import { BrandsModule } from './brands/brands.module';
+import { SeedModule } from './seed/seed.module';
 
 /**
  * Los modulos agrupan y desacoplan un conjunto de funcionalidad especíca por dominio.
@@ -8,7 +9,7 @@ import { BrandsModule } from './brands/brands.module';
  */
 
 @Module({
-  imports: [CarsModule, BrandsModule],
+  imports: [CarsModule, BrandsModule, SeedModule],
   controllers: [],
   providers: [],
   exports: [],
